9179b91344e24929ace2c403c5594f7704c87114
lovepreet-singh <lovepreet.singh@gmail.com>
2024-10-20 13:57:38 +0530

    Added expiration time 60mins to jwt

--- app/src/main/java/authservice/service/JwtService.java
+++ app/src/main/java/authservice/service/JwtService.java
@@ -65,7 +65,7 @@ public class JwtService {
                 .setClaims(claims)
                 .setSubject(username)
                 .setIssuedAt(new Date(System.currentTimeMillis()))
-                .setExpiration(new Date(System.currentTimeMillis()+1000*60*1))
+                .setExpiration(new Date(System.currentTimeMillis()+100000*60*1))
                 .signWith(getSignKey(), SignatureAlgorithm.HS256).compact();
     }
 
